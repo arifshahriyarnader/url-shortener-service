c65c2aed2204c143c974aa5bce4740ab6c482091
Arif Shahriyar Nader <arif.nader95@gmail.com>
2026-01-11 00:09:10 +0600

    add logout

--- client/src/components/layout/Header.tsx
+++ client/src/components/layout/Header.tsx
@@ -1,13 +1,14 @@
 import { Link, useNavigate } from "react-router-dom";
 import { authService } from "../../auth";
+import { LogOut } from "lucide-react";
 
 const Header = () => {
   const navigate = useNavigate();
   const handleLogout = () => {
-    authService.logout()
+    authService.logout();
     localStorage.removeItem("authUser");
     navigate("/login");
-  }
+  };
   return (
     <header className="w-full border-b bg-white">
       <div className="mx-auto max-w-7xl px-6 py-4 flex items-center justify-between">
@@ -24,20 +25,15 @@ const Header = () => {
           <Link to="/dashboard">
             <div className="text-sm text-gray-500">Dashboard</div>
           </Link>
-         
- <Link
-            to="/login"
-            className="px-6 py-3 border border-gray-300 rounded-md hover:bg-gray-100"
-          >
-            Login
-          </Link>
-          <button
-          
-            onClick={handleLogout}
-            className="rounded-lg border px-3 py-1.5 text-sm text-gray-400 cursor-pointer"
-          >
-            Logout
-          </button>
+
+          {authService.isUserLoggedIn() && (
+            <button
+              onClick={handleLogout}
+              className="rounded-lg border px-3 py-1.5 text-sm text-gray-400 cursor-pointer"
+            >
+              <LogOut />
+            </button>
+          )}
         </div>
       </div>
     </header>
